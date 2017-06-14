b6b28cecaa4e6e7f527ffb420dd5ba00ab7bdfcd
qq <qq@qqtekiMacBook-Air.local>
2017-06-14 14:53:34 +0800

    side

--- WebContent/web/src/components/income/IncomeDetail.jsx
+++ WebContent/web/src/components/income/IncomeDetail.jsx
@@ -15,24 +15,28 @@ const dateFormat = 'YYYY/MM/DD';
 //表格
 const data = [{
     key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
+    index:'1',
+    time: 'John Brown',
+    actualIncome: 32,
+    actualPay: 'New York No. 1 Lake Park',
 }, {
     key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
+    index:'2',
+    time: 'John Brown',
+    actualIncome: 32,
+    actualPay: 'New York No. 1 Lake Park',
 }, {
     key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
+    index:'3',
+    time: 'John Brown',
+    actualIncome: 32,
+    actualPay: 'New York No. 1 Lake Park',
 }, {
     key: '4',
-    name: 'Jim Red',
-    age: 32,
-    address: 'London No. 2 Lake Park',
+    index:'4',
+    time: 'John Brown',
+    actualIncome: 32,
+    actualPay: 'New York No. 1 Lake Park',
 }];
 
 class BeautyOrder extends React.Component {
@@ -45,8 +49,6 @@ class BeautyOrder extends React.Component {
             loading: false,
         }
     }
-
-
     handleChange = (pagination, filters, sorter) => {
         console.log('Various parameters', pagination, filters, sorter);
         this.setState({
@@ -78,35 +80,21 @@ class BeautyOrder extends React.Component {
         sortedInfo = sortedInfo || {};
         filteredInfo = filteredInfo || {};
         const columns = [{
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            filters: [
-                { text: 'Joe', value: 'Joe' },
-                { text: 'Jim', value: 'Jim' },
-            ],
-            filteredValue: filteredInfo.name || null,
-            onFilter: (value, record) => record.name.includes(value),
-            sorter: (a, b) => a.name.length - b.name.length,
-            sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
+            title:'序号',
+            dataIndex:'index',
+            key:'index'
+        },{
+            title: '时间',
+            dataIndex: 'time',
+            key: 'time'
         }, {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
-            sorter: (a, b) => a.age - b.age,
-            sortOrder: sortedInfo.columnKey === 'age' && sortedInfo.order,
+            title: '实收金额',
+            dataIndex: 'actualIncome',
+            key: 'actualMoney'
         }, {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-            filters: [
-                { text: 'London', value: 'London' },
-                { text: 'New York', value: 'New York' },
-            ],
-            filteredValue: filteredInfo.address || null,
-            onFilter: (value, record) => record.address.includes(value),
-            sorter: (a, b) => a.address.length - b.address.length,
-            sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
+            title: '实际支出',
+            dataIndex: 'actualPay',
+            key: 'actualPay'
         }];
         return (
             <div>
@@ -117,7 +105,7 @@ class BeautyOrder extends React.Component {
                         <Button onClick={this.clearFilters}>本周</Button>
                         <Button onClick={this.clearAll}>本月</Button>
                     </div>
-                    <Table columns={columns} dataSource={data} onChange={this.handleChange} />
+                    <Table bordered columns={columns} dataSource={data} onChange={this.handleChange} />
                 </Card>
             </div>
         );
