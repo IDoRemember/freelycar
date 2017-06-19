d4856bc980be1ca7f0d203b0eb728a2fea20a307
mxy940127 <634666730@qq.com>
2017-06-19 17:57:24 +0800

    staff接口

--- src/com/geariot/platform/freelycar/controller/StaffController.java
+++ src/com/geariot/platform/freelycar/controller/StaffController.java
@@ -1,38 +1,45 @@
 package com.geariot.platform.freelycar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.geariot.platform.freelycar.entities.Staff;
+import com.geariot.platform.freelycar.service.StaffService;
+import com.geariot.platform.freelycar.shiro.PermissionRequire;
 
 @RestController
 @RequestMapping(value = "/staff")
 public class StaffController {
 
+	@Autowired
+	private StaffService staffService;
+	
 	@RequestMapping(value = "/add" , method = RequestMethod.POST)
+	@PermissionRequire
 	public String addStaff(Staff staff){
-		return null;
+		return staffService.addStaff(staff);
 	}
 	
 	@RequestMapping(value = "/modify" , method = RequestMethod.POST)
 	public String modifyStaff(Staff staff){
-		return null;
+		return staffService.modifyStaff(staff);
 	}
 	
 	@RequestMapping(value = "/delete" , method = RequestMethod.POST)
-	public String deleteStaff(int staffId){
-		return null;
+	public String deleteStaff(int[] staffIds ){
+		return staffService.deleteStaff(staffIds);
 	}
 	
 	@RequestMapping(value = "/list" , method = RequestMethod.GET)
 	public String getStaffList(int page , int number){
-		return null;
+		return staffService.getStaffList(page, number);
 	}
 	
 	@RequestMapping(value = "/query" , method = RequestMethod.GET)
 	public String getSelectStaff(int staffId , String staffName){
-		return null;
+		return staffService.getSelectStaff(staffId, staffName);
 	}
 	
 }
