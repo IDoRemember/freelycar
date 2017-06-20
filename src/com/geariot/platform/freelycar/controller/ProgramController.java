5832e8125a00c212421f50b17d0a1cc96a2d40ed
mxy940127 <634666730@qq.com>
2017-06-20 16:25:27 +0800

    program后台接口实现

--- src/com/geariot/platform/freelycar/controller/ProgramController.java
+++ src/com/geariot/platform/freelycar/controller/ProgramController.java
@@ -1,28 +1,33 @@
 package com.geariot.platform.freelycar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.geariot.platform.freelycar.entities.Program;
+import com.geariot.platform.freelycar.service.ProgramService;
 
 @RestController
 @RequestMapping(value = "/program")
 public class ProgramController {
 
+	@Autowired
+	private ProgramService programService;
+	
 	@RequestMapping(value = "/add" , method = RequestMethod.POST)
 	public String addProgram(Program program){
-		return null;
+		return programService.addProgram(program);
 	}
 	
 	@RequestMapping(value = "/list" , method = RequestMethod.GET)
 	public String getProgramList(int page , int number){
-		return null;
+		return programService.getProgramList(page, number);
 	}
 	
 	@RequestMapping(value = "/delete" , method = RequestMethod.POST)
 	public String deleteProgram(int programId){
-		return null;
+		return programService.deleteProgram(programId);
 	}
 	
 }
---
+++ src/com/geariot/platform/freelycar/service/ProgramService.java
@@ -0,0 +1,78 @@
+package com.geariot.platform.freelycar.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.geariot.platform.freelycar.dao.ProgramDao;
+import com.geariot.platform.freelycar.dao.ProjectDao;
+import com.geariot.platform.freelycar.entities.Program;
+import com.geariot.platform.freelycar.model.RESCODE;
+import com.geariot.platform.freelycar.utils.Constants;
+import com.geariot.platform.freelycar.utils.DateJsonValueProcessor;
+import com.geariot.platform.freelycar.utils.JsonResFactory;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JsonConfig;
+
+@Service
+@Transactional
+public class ProgramService {
+
+	@Autowired
+	private ProgramDao programDao;
+	
+	@Autowired
+	private ProjectDao projectDao;
+	
+	public String addProgram(Program program){
+		Program exist = programDao.findProgramByName(program.getName());
+		JSONObject obj = null;
+		if(exist != null){
+			obj = JsonResFactory.buildOrg(RESCODE.ACCOUNT_EXIST);
+		}
+		else {
+			program.setCreateDate(new Date());
+			programDao.save(program);
+			obj = JsonResFactory.buildOrg(RESCODE.SUCCESS);
+		}
+		return obj.toString();
+	}
+	
+	public String getProgramList(int page , int number){
+		int from = (page - 1) * number;
+		List<Program> list = programDao.listPrograms(from, number);
+		if(list == null || list.isEmpty()){
+			return JsonResFactory.buildOrg(RESCODE.NOT_FOUND).toString();
+		}
+		long realSize = programDao.getCount();
+		int size=(int) Math.ceil(realSize/(double)number);
+		JsonConfig config = new JsonConfig();
+		config.registerJsonValueProcessor(Date.class, new DateJsonValueProcessor());
+		JSONArray jsonArray = JSONArray.fromObject(list, config);
+		net.sf.json.JSONObject obj = JsonResFactory.buildNetWithData(RESCODE.SUCCESS, jsonArray);
+		obj.put(Constants.RESPONSE_SIZE_KEY, size);
+		return obj.toString();
+	}
+	
+	public String deleteProgram(int programId){
+		Program exist = programDao.findProgramByProgramId(programId);
+		JSONObject obj = null;
+		if(exist == null){
+			obj = JsonResFactory.buildOrg(RESCODE.NOT_FOUND);
+		}
+		else{
+			programDao.delete(programId);
+			projectDao.deleteByprogramId(programId);
+			obj = JsonResFactory.buildOrg(RESCODE.SUCCESS);
+		}
+		return obj.toString();
+	}
+	
+}
