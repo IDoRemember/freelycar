21caf5f1b45b9372ebdba1cb6629c16a562c38ed
Tangqun Zha <378379092@qq.com>
2017-06-21 16:23:01 +0800

    首页

--- WebContent/web/src/components/IndexPage.jsx
+++ WebContent/web/src/components/IndexPage.jsx
@@ -8,16 +8,67 @@ class IndexPage extends React.Component {
         return (
             <div style={{ background: '#ECECEC' }}>
                  <BreadcrumbCustom />
-                <Row gutter={16}>
-                    <Col span={8}>
-                        <Card  bordered={false}>Card content</Card>
+                <Row gutter={16} style={{width:'80%',marginLeft:'10%'}}>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#a9c3e4'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">消费开单</span>
+                            </div>
+                            </Card>
                     </Col>
-                    <Col span={8}>
-                        <Card  bordered={false}>Card content</Card>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#eafd89'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">收支管理</span>
+                            </div>
+                            </Card>
                     </Col>
-                    <Col span={8}>
-                        <Card  bordered={false}>Card content</Card>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'lightblue'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">会员管理</span>
+                            </div>
+                            </Card>
                     </Col>
+                    <Col span={6} >
+                        <Card  bordered={false} style={{backgroundColor:'#a3c07e'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">进销存管理</span>
+                            </div>
+                            </Card>
+                    </Col>
+                  
+                </Row>
+                  <Row gutter={16} style={{width:'80%',marginLeft:'10%',marginTop:'20px'}}>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#ff9b6d'}}>
+                            <div style={{height:'150px',textAlign:'center'}} >
+                                <span className="cardtext">产品管理</span>
+                            </div>
+                            </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#ffd37c'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">数据报表</span>
+                            </div>
+                            </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#aeaeae'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <span className="cardtext">系统设置</span>
+                            </div>
+                            </Card>
+                    </Col>
+                    <Col span={6}>
+                        <Card  bordered={false} style={{backgroundColor:'#f8e4dd'}}>
+                            <div style={{height:'150px',textAlign:"center"}}>
+                                <Icon type="plus-circle-o" style={{fontSize:'100px',color:'#fff',lineHeight:'150px'}} />
+                            </div>
+                            </Card>
+                    </Col>
+                  
                 </Row>
             </div>
         )
