e6f45353228f15609d5573df49629ce5fbd7cefb
mxy940127 <634666730@qq.com>
2017-06-27 10:10:06 +0800

    provider提供当前总数

--- src/com/geariot/platform/freelycar/service/ProviderService.java
+++ src/com/geariot/platform/freelycar/service/ProviderService.java
@@ -29,10 +29,12 @@ public class ProviderService {
 	public String addProvider(Provider provider){
 		provider.setCreateDate(new Date());
 		providerDao.save(provider);
+		long realSize = providerDao.getCount();
 		JsonConfig config = new JsonConfig();
 		config.registerJsonValueProcessor(Date.class, new DateJsonValueProcessor());
 		JSONObject data = JSONObject.fromObject(provider , config);
 		net.sf.json.JSONObject obj = JsonResFactory.buildNetWithData(RESCODE.SUCCESS, data);
+		obj.put(Constants.RESPONSE_REAL_SIZE_KEY, realSize);
 		return obj.toString();
 		/*provider.setCreateDate(new Date());
 		providerDao.save(provider);
