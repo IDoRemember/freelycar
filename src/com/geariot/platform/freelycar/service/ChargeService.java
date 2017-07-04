341e210f33ae2e07135bcf653f97e66e71a21f77
mxy940127 <634666730@qq.com>
2017-07-04 11:42:20 +0800

    name

--- src/com/geariot/platform/freelycar/service/ChargeService.java
+++ src/com/geariot/platform/freelycar/service/ChargeService.java
@@ -74,8 +74,10 @@ public class ChargeService {
 	public String addCharge(OtherExpendOrder otherExpendOrder){
 		otherExpendOrder.setId(IDGenerator.generate(IDGenerator.CHARORDER_ID));
 		otherExpendOrder.setCreateDate(new Date());
+		OtherExpendType type = chargeDao.findById(otherExpendOrder.getType().getId());
+		otherExpendOrder.setType(type);
 		chargeDao.save(otherExpendOrder);
-		System.out.println(otherExpendOrder.toString());
+		//System.out.println(otherExpendOrder.toString());
 		//当有其他支出发生时,取信息存入expendOrder表
 		ExpendOrder expendOrder = new ExpendOrder();
 		expendOrder.setAmount(otherExpendOrder.getAmount());
