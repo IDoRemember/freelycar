829ae80aa846630d2801b593858d30814f867222
mxy940127 <634666730@qq.com>
2017-07-05 16:59:54 +0800

    取消

--- src/com/geariot/platform/freelycar/service/ServiceService.java
+++ src/com/geariot/platform/freelycar/service/ServiceService.java
@@ -32,7 +32,6 @@ public class ServiceService {
 		service.setCreateDate(new Date());
 		serviceDao.save(service);
 		JsonConfig config = JsonResFactory.dateConfig();
-		//sys
 		return JsonResFactory.buildNetWithData(RESCODE.SUCCESS,net.sf.json.JSONObject.fromObject(service, config)).toString();
 	}
 	
