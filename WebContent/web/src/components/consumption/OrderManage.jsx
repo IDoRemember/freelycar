93d6a4aae54fb6476cb8980d6b1fd8436f631991
IDoRemember <1015885915@qq.com>
2017-07-10 15:02:36 +0800

    单据管理

--- WebContent/web/src/components/consumption/OrderManage.jsx
+++ WebContent/web/src/components/consumption/OrderManage.jsx
@@ -4,6 +4,7 @@ import BreadcrumbCustom from '../BreadcrumbCustom.jsx';
 import { Link } from 'react-router';
 import OrderTable from './OrderTable.jsx'
 import $ from 'jquery'
+import update from 'immutability-helper'
 const { MonthPicker, RangePicker } = DatePicker;
 const Option = Select.Option;
 class OrderManage extends React.Component {
@@ -12,7 +13,16 @@ class OrderManage extends React.Component {
         this.state = {
             option: [],
             type: '美容',
-            data:[]
+            data: [],
+            query: {
+                id: null,
+                licensePlate: '',
+                programId: null,
+                payState: '',
+                startDate: '',
+                endDate: '',
+                dataType: null,
+            }
         }
     }
     handleSelectChange = (value) => {
@@ -30,18 +40,68 @@ class OrderManage extends React.Component {
             // contentType:'application/json;charset=utf-8',
             dataType: 'json',
             data: {
-                page:1,
-                number:10
+                page: 1,
+                number: 10
             },
-            success:(res)=>{
-                if(res.code=='0') {
+            success: (res) => {
+                if (res.code == '0') {
                     let dataArray = res.data
                     for (let item of dataArray) {
                         item.key = item.id
                     }
-                   this.setState({
-                       data:dataArray
-                   }) 
+                    this.setState({
+                        data: dataArray
+                    })
+                }
+            }
+        })
+    }
+
+    setQueryData = (key, data) => {
+        this.setState({
+            query: update(this.state.query, { [key]: { $set: data } })
+        })
+    }
+    startClear = () => {
+        this.setState({
+            query: {
+                id: null,
+                licensePlate: '',
+                programId: null,
+                payState: '',
+                startDate: '',
+                endDate: '',
+                dataType: null,
+            }
+        })
+    }
+    getQuery = () => {
+        $.ajax({
+            url: 'api/order/query',
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify({
+                ConsumOrder: {
+                    id: this.state.query.id,
+                    licensePlate: this.state.query.licensePlate,
+                    programId: this.state.query.programId,
+                    payState: this.state.query.payState
+                },
+                startDate: this.state.query.startDate,
+                endDate: this.state.query.endDate,
+                dateType: this.state.query.dataType,
+                page: 1,
+                number: 10
+            }),
+            success: (res) => {
+                if (res.code == '0') {
+                    let dataArray = res.data
+                    for (let item of dataArray) {
+                        item.key = item.id
+                    }
+                    this.setState({
+                        data: dataArray
+                    })
                 }
             }
         })
@@ -61,21 +121,13 @@ class OrderManage extends React.Component {
                     <Row gutter={16} style={{ marginBottom: "10px" }}>
                         <Col span={8} >
                             单据编号：
-                        <Select showSearch
-                                style={{ width: '100px' }}
-                                placeholder="输入单据编号"
-                                optionFilterProp="children"
-                                onChange={this.handleChange}
-                                filterOption={(input, option) => option.props.children.indexOf(input) >= 0}
-                            >
-                                {plateOptions}
-                            </Select>
+                       <Input style={{ width: '120px' }} onChange={(e) => this.setQueryData('id', e.target.value)} />
                         </Col>
-                        <Col span={8} >
+                        <Col span={8} id="type">
                             项目类别：
-                        <Select defaultValue="美容" style={{ width: 120 }} onChange={this.handleSelectChange}>
-                                <Option value="美容">美容</Option>
-                                <Option value="维修">维修</Option>
+                        <Select style={{ width: 120 }} onChange={(value) => this.setQueryData('programId', value)} getPopupContainer={() => document.getElementById('type')}>
+                                <Option value="1">美容</Option>
+                                <Option value="2">维修</Option>
                             </Select>
                         </Col>
                         <Col span={8} >
@@ -88,52 +140,43 @@ class OrderManage extends React.Component {
                         </Col>
                     </Row>
                     <Row gutter={16} style={{ marginBottom: "10px" }}>
-                        <Col span={8} >
+                        <Col span={8} id="car-number">
                             车牌号码：
-                         <Select showSearch
-                                style={{ width: '100px' }}
-                                placeholder="输入单据编号"
-                                optionFilterProp="children"
-                                onChange={this.handleChange}
-                                filterOption={(input, option) => option.props.children.indexOf(input) >= 0}
-                            >
-                                {plateOptions}
-                            </Select>
+                         <Input style={{ width: '120px' }} onChange={(e) => this.setQueryData('licensePlate', e.target.value)} />
                         </Col>
-                        <Col span={8} >
+                        <Col span={8} id="pay-state">
                             结算状态：
-                        <Select defaultValue="nonPayment" style={{ width: 120 }} onChange={this.handleChange}>
-                                <Option value="nonPayment">挂单中</Option>
-                                <Option value=" Paid">已结算</Option>
+                        <Select style={{ width: 120 }} onChange={(value) => this.setQueryData('payState', value)} getPopupContainer={() => document.getElementById('pay-state')}>
+                                <Option value="0">挂单中</Option>
+                                <Option value=" 1">已结算</Option>
                             </Select>
                         </Col>
-                        <Col span={8} />
                     </Row>
-                    <Row gutter={16} style={{ marginBottom: "10px" }}>
+                    <Row gutter={16} style={{ marginBottom: "10px" }} id="area">
                         <Col span={8} >
                             时间类型：
-                        <Select defaultValue="单据时间" style={{ width: 120 }} onChange={this.handleChange}>
-                                <Option value="receiptsTime">单据时间</Option>
-                                <Option value="acceptTime">接车时间</Option>
-                                <Option value="handTime">交车时间</Option>
-                                <Option value="completeTime">完工时间</Option>
+                        <Select defaultValue="单据时间" style={{ width: 120 }} onChange={(value) => this.setQueryData('dateType', value)} getPopupContainer={() => document.getElementById('area')}>
+                                <Option value="0">单据时间</Option>
+                                <Option value="1">接车时间</Option>
+                                <Option value="2">交车时间</Option>
+                                <Option value="3">完工时间</Option>
                             </Select>
                         </Col>
-                        <Col span={8} >
+                        <Col span={8} id="timepicker">
                             <div>
-                                <RangePicker onChange={onChange} />
+                                <RangePicker onChange={onChange} getPopupContainer={() => document.getElementById('timepicker')} />
                             </div>
                         </Col>
                         <Col span={8} />
                     </Row>
                     <Row gutter={16} style={{ marginBottom: "10px" }}>
                         <Col span={8}>
-                            <Button type="primary">查询</Button>
-                            <Button type="primary">清空</Button>
+                            <Button type="primary" onClick={() => this.getQuery}>查询</Button>
+                            <Button type="primary" onClick={() => this.startClear}>清空</Button>
                         </Col>
                     </Row>
                 </Card>
-                <OrderTable data={this.state.data}/>
+                <OrderTable data={this.state.data} />
             </div>
         )
     }
