7876ba87c28879e250b473d084e47314e26502d5
Javebean <448267696@qq.com>
2017-07-10 17:43:07 +0800

    开单

--- WebContent/web/src/components/tables/PartsDetail.jsx
+++ WebContent/web/src/components/tables/PartsDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Card, Select, Table, Iconconst, Popconfirm,InputNumber } from 'antd';
+import { Row, Col, Card, Select, Table, Iconconst, Popconfirm, InputNumber } from 'antd';
 import AjaxGet from '../../utils/ajaxGet'
 import AjaxSend from '../../utils/ajaxSend'
 import EditableCell from './EditableCell.jsx'
@@ -13,11 +13,12 @@ class PartsDetail extends React.Component {
             data: [{
                 key: 1,
                 index: 1,
-                name: '洗车',
+                name: '',
                 brandName: '',
                 price: '20.00',
                 number: '20',
                 amount: '0',
+                singleSummation: '0',
                 standard: '',
             }, {
                 key: '',
@@ -35,11 +36,15 @@ class PartsDetail extends React.Component {
             type: 'get',
             dataType: 'json',
             success: (res) => {
-                console.log(res);
+                //console.log(res);
                 if (res.code == '0') {
                     this.setState({
                         option: res.data
                     });
+                    //为配件名称下拉第一个赋值
+                    this.setState({
+                        data: update(this.state.data, { [0]: { name: { $set: res.data[0].name } } })
+                    })
                 }
             }
 
@@ -54,7 +59,7 @@ class PartsDetail extends React.Component {
             data: { inventoryId: value },
             dataType: 'json',
             success: (res) => {
-                console.log(res);
+                //console.log(res);
                 if (res.code == '0') {
                     let obj = res.data;
                     obj.key = obj.id;
@@ -72,12 +77,12 @@ class PartsDetail extends React.Component {
     }
 
     numberChange = (index, value) => {
-        console.log(index, value);
+        //console.log(index, value);
         let price = this.state.data[index].price;
         this.setState({
             data: update(this.state.data, { [index]: { singleSummation: { $set: price * value } } })
         }, () => {
-            console.log(this.state.data);
+            // console.log(this.state.data);
         })
     }
 
@@ -87,10 +92,10 @@ class PartsDetail extends React.Component {
             key: this.state.data.length,
             index: this.state.data.length,
             project: 'xiuche',
-            price: '30.00',
-            number: '20',
-            singleSummation: '20',
-            DeductionCardTime: '1',
+            price: '0',
+            number: '1',
+            singleSummation: '0',
+            DeductionCardTime: '0',
         }
         let data = this.state.data
         data.splice(data.length - 1, 0, oneRow)
@@ -133,14 +138,17 @@ class PartsDetail extends React.Component {
                     key="name"
                     dataIndex="name"
                     render={(text, record, index) => {
-                        return <Select showSearch
-                            style={{ width: '100px' }}
-                            placeholder="输入配件名称"
-                            defaultValue={this.state.data[index].name}
-                            onSelect={(e) => this.handleInvChange(index, e)}
-                        >
-                            {projectOptions}
-                        </Select>
+                        if (index + 1 < this.state.data.length) {
+                            return <Select showSearch
+                                style={{ width: '100px' }}
+                                placeholder="输入配件名称"
+                                defaultValue={this.state.data[index].name}
+                                value={this.state.data[index].name}
+                                onSelect={(e) => this.handleInvChange(index, e)}
+                            >
+                                {projectOptions}
+                            </Select>
+                        }
 
                     }}
                 />
@@ -175,7 +183,9 @@ class PartsDetail extends React.Component {
                     key="number"
                     dataIndex="number"
                     render={(text, record, index) => {
-                        return <InputNumber min={1} max={99} defaultValue={1} onChange={(e) => { this.numberChange(index, e) }} />
+                        if (index + 1 < this.state.data.length) {
+                            return <InputNumber min={1} max={99} defaultValue={1} onChange={(e) => { this.numberChange(index, e) }} />
+                        }
                     }}
                 />
                 <Col
