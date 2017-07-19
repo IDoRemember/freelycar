2b9f4503e81e07c8cf067f54778023ae295c7246
Javebean <448267696@qq.com>
2017-07-19 09:27:39 +0800

    生产环境

---
+++ WebContent/web/webpack.production.config.js
@@ -0,0 +1,44 @@
+
+'use strict';
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
+module.exports = {
+    entry: __dirname + '/src/entry.js', //唯一入口文件
+    output: {
+        path: __dirname + '/build', //打包后的文件存放的地方
+        filename: 'bundle.js', //打包后输出文件的文件名
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react', 'stage-1'],
+                    plugins: [
+                        ['import', [{ libraryName: "antd", style: true }]],
+                    ]
+                }
+            },
+            { test: /\.json$/, loader: 'json-loader' },
+            { test: /\.(css|less)$/, loader: ExtractTextPlugin.extract("style", "css!less") },
+            { test: /\.(png|jpg)$/, loader: 'url?limit=8192' }
+        ]
+    },
+
+    postcss: [
+        require('autoprefixer')    //调用autoprefixer插件,css3自动补全
+    ],
+
+    plugins: [
+        new ExtractTextPlugin('main.css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify("production"),
+            },
+        }),
+    ]
+
+}
