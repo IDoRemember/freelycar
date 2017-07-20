f32028d0bbf8439f28d54fbd391307baeee2b0ab
IDoRemember <1015885915@qq.com>
2017-07-20 15:34:47 +0800

    美容开单

--- WebContent/web/src/components/consumption/BeautyOrder.jsx
+++ WebContent/web/src/components/consumption/BeautyOrder.jsx
@@ -233,7 +233,7 @@ class BeautyOrder extends React.Component {
                     元
                 </div>
             </Card>
-            <Button type="primary" style={{ float: 'right', margin: '10px', width: '100px', height: '50px' }} size={'large'}><Link to={this.state.id ? `/app/consumption/accountingcenter/${this.state.id}` : ''}>结算</Link></Button>
+            <Button type="primary" style={{ float: 'right', margin: '10px', width: '100px', height: '50px' }} size={'large'}><Link to={this.state.id!='' ? `/app/consumption/accountingcenter/${this.state.id}` : ''}>结算</Link></Button>
             <Button type="primary" style={{ float: 'right', margin: '10px', width: '100px', height: '50px' }} size={'large'} onClick={() => { this.book() }}>保存</Button>
             <Button type="primary" style={{ float: 'right', margin: '10px', width: '100px', height: '50px' }} size={'large'}>重新开单</Button>
         </div>
