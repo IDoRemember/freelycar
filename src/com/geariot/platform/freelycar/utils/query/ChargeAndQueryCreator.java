5c09eb5f5e2184af9875575f687d2eda3b34ab1a
ABC <ABC@ABC-PC>
2017-07-24 16:52:53 +0800

    commit

--- src/com/geariot/platform/freelycar/utils/query/ChargeAndQueryCreator.java
+++ src/com/geariot/platform/freelycar/utils/query/ChargeAndQueryCreator.java
@@ -7,13 +7,13 @@ public class ChargeAndQueryCreator extends AndQueryCreator {
 	@Override
 	protected void init() {
 		conditionKeys = new ArrayList<ConBean>();
-		conditionKeys.add(new ConBean("otherExpendTypeId", "=="));
+		conditionKeys.add(new ConBean("typeId", "=="));
     	conditionKeys.add(new ConBean("expendDate", ">="));
     	conditionKeys.add(new ConBean("expendDate", "<="));
 	}
 
-	public ChargeAndQueryCreator(String otherExpendTypeId, String startTime, String endTime){
-		super(otherExpendTypeId, startTime, endTime);
+	public ChargeAndQueryCreator(String typeId, String startTime, String endTime){
+		super(typeId, startTime, endTime);
 	}
 	
 }
